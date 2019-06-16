de7560ef02421c1514fbfead1903fb9b50f33757
iBowie <31174978+iBowie@users.noreply.github.com>
2019-06-17 01:05:47 +0500

    project structure changed

---
+++ NPCMakerTests/ApparelTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BowieD.Unturned.NPCMaker.NPC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NPCMakerTests
+{
+    [TestClass]
+    public class ApparelTest
+    {
+        [TestMethod]
+        public void InputEveryColor()
+        {
+            for (byte r = 0; r < 255; r++)
+            {
+                for (byte g = 0; g < 255; g++)
+                {
+                    for (byte b = 0; b < 255; b++)
+                    {
+                        NPCColor color = new NPCColor(r, g, b);
+                        string HEX = color.HEX;
+                        var HSV = color.HSV;
+                    }
+                }
+            }
+        }
+        [TestMethod]
+        public void InputRandomColor()
+        {
+            byte[] arr = new byte[3];
+            System.Security.Cryptography.RandomNumberGenerator.Create().GetBytes(arr);
+            NPCColor color = new NPCColor(arr[0], arr[1], arr[2]);
+            string hex = color.HEX;
+            var hsv = color.HSV;
+        }
+        [TestMethod]
+        public void ReverseColorConvert()
+        {
+            byte[] arr = new byte[3];
+            System.Security.Cryptography.RandomNumberGenerator.Create().GetBytes(arr);
+            NPCColor color = new NPCColor(arr[0], arr[1], arr[2]);
+            string hex = color.HEX;
+            NPCColor fromHex = new NPCColor() { HEX = hex };
+            var hsv = color.HSV;
+            NPCColor fromHSV = NPCColor.FromHSV(hsv.Item1, hsv.Item2, hsv.Item3);
+            if (color != fromHex || color != fromHSV)
+            {
+                throw new Exception("Цвета различаются после конвертации");
+            }
+        }
+    }
+}
---
+++ NPCMakerTests/ExportTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BowieD.Unturned.NPCMaker.NPC;
+using BowieD.Unturned.NPCMaker.NPC.Conditions;
+using BowieD.Unturned.NPCMaker.NPC.Rewards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NPCMakerTests
+{
+    [TestClass]
+    public class ExportTest
+    {
+        [TestMethod]
+        public void ExportFuelObject()
+        {
+            NPCObject fuelObject = new NPCObject()
+            {
+                interactability = Object_Interactability.Fuel,
+                interactResource = 50,
+                interactReset = 60,
+                name = "Tank",
+                ID = 25000,
+                hint = Object_Hint.None,
+                assetFilePath = @"G:\Old Drive\Sorted Shit\Unturned\Workshop\BMA_UI\BMA_UI\Bundles\Effects\BMA_UI\BMA_UI.unity3d"
+            };
+            NPCSave save = new NPCSave()
+            {
+                objects = new System.Collections.Generic.List<NPCObject>() { fuelObject },
+                guid = Guid.NewGuid().ToString("N")
+            };
+            BowieD.Unturned.NPCMaker.Export.Exporter.ExportNPC(save);
+        }
+        [TestMethod]
+        public void ExportQuestObject()
+        {
+            NPCObject questObject = new NPCObject()
+            {
+                interactability = Object_Interactability.Quest,
+                interactEffect = 126,
+                rewards = new System.Collections.Generic.List<Reward>()
+                {
+                    new Experience()
+                    {
+                        Value = 50,
+                        Type = RewardType.Experience
+                    },
+                    new Experience()
+                    {
+                        Value = 25,
+                        Type = RewardType.Experience
+                    },
+                    new Quest()
+                    {
+                        Id = 25001,
+                        Type = RewardType.Quest
+                    }
+                },
+                ID = 25000,
+                assetFilePath = @"G:\Old Drive\Sorted Shit\Unturned\Workshop\BMA_UI\BMA_UI\Bundles\Effects\BMA_UI\BMA_UI.unity3d",
+                conditions = new System.Collections.Generic.List<Condition>()
+                {
+                    new Quest_Cond()
+                    {
+                        Id = 25001,
+                        Logic = Logic_Type.Equal,
+                        Status = Quest_Status.None,
+                        Reset = false
+                    }
+                },
+                type = Object_Type.LARGE
+            };
+            NPCSave save = new NPCSave()
+            {
+                objects = new System.Collections.Generic.List<NPCObject>() { questObject },
+                guid = Guid.NewGuid().ToString("N")
+            };
+            BowieD.Unturned.NPCMaker.Export.Exporter.ExportNPC(save);
+        }
+    }
+}
