42102bc18e9dab39a0413da43ecc77136682638c
iBowie <31174978+iBowie@users.noreply.github.com>
2019-07-30 19:40:45 +0500

    Create Project.cs

---
+++ BowieD.NPCMaker/Storage/Project.cs
@@ -0,0 +1,14 @@
+﻿using BowieD.NPCMaker.NPC;
+using System.Collections.Generic;
+
+namespace BowieD.NPCMaker.Storage
+{
+    public class Project
+    {
+        public string name;
+        public List<Character> characters;
+        public List<Dialogue> dialogues;
+        public List<Vendor> vendors;
+        public List<Quest> quests;
+    }
+}
