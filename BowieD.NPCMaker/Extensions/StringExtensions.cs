8dd06692914bfb83ce547ad36d7b17ec39bb2b02
iBowie <31174978+iBowie@users.noreply.github.com>
2019-07-30 20:01:40 +0500

    Create StringExtensions.cs

---
+++ BowieD.NPCMaker/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace BowieD.NPCMaker.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string origin)
+        {
+            switch (origin)
+            {
+                case null:
+                case "":
+                    return origin;
+                default:
+                    return char.ToUpper(origin[0]) + new string(origin.Skip(1).Select(d => char.ToLower(d)).ToArray());
+            }
+        }
+    }
+}
