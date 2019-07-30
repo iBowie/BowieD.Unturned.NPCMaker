73b857553ed5b0b748b0b9b5ed60c36cad778aee
iBowie <31174978+iBowie@users.noreply.github.com>
2019-07-30 20:15:28 +0500

    Create ArrayExtensions.cs

---
+++ BowieD.NPCMaker/Extensions/ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BowieD.NPCMaker.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this Array array)
+        {
+            foreach (var k in array)
+            {
+                yield return (T)k;
+            }
+        }
+    }
+}
