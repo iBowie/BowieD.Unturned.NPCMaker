6159fb69cdaa3ab83ffecb83942dac49c7fe3095
iBowie <31174978+iBowie@users.noreply.github.com>
2019-08-01 15:13:38 +0500

    added character and condition export tests

--- BowieD.NPCMaker/App.xaml.cs
+++ BowieD.NPCMaker/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BowieD.NPCMaker.Storage;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -16,7 +17,66 @@ namespace BowieD.NPCMaker
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+        }
 
+        public void runCharacterExportTest()
+        {
+            Project proj = new Project();
+            proj.characters.Add(new NPC.Character()
+            {
+                beard = 1,
+                christmasClothing = new NPC.Outfit(),
+                defaultClothing = new NPC.Outfit()
+                {
+                    shirt = 250,
+                    pants = 251
+                },
+                dialogueId = 25,
+                id = 24,
+                displayName = new Dictionary<NPC.ELanguage, string>()
+                {
+                    { NPC.ELanguage.English, "Export Display Test" },
+                    { NPC.ELanguage.Russian, "Тест Экспорта Видимое имя" }
+                },
+                editorName = new Dictionary<NPC.ELanguage, string>()
+                {
+                    { NPC.ELanguage.English, "Export Editor Test" },
+                    { NPC.ELanguage.Russian, "Тест Экспорта Имя в редакторе" }
+                },
+                equippedSlot = NPC.ESlotType.NONE,
+                equipPrimary = 0,
+                equipSecondary = 0,
+                equipTertiary = 0,
+                face = 1,
+                hair = 5,
+                hairColor = new Coloring.Color(0,0,0),
+                skinColor = new Coloring.Color(0,0,0),
+                halloweenClothing = new NPC.Outfit(),
+                leftHanded = false,
+                pose = NPC.ENPCPose.STAND,
+                visibilityConditions = new List<NPC.Condition.Condition>()
+            });
+            Export.ExportTool.ExportCharacter(proj.characters[0], $@"K:\User\Desktop\npctest\");
+        }
+        public void runConditionExportTest()
+        {
+            NPC.Condition.Condition cnd = new NPC.Condition.ConditionCompareFlags()
+            {
+                Allow_A_Unset = false,
+                Allow_B_Unset = true,
+                A_ID = 1,
+                B_ID = 2,
+                localization = new Dictionary<NPC.ELanguage, string>()
+                {
+                    { NPC.ELanguage.English, "no text" }
+                },
+                Logic = NPC.ENPCLogicType.EQUAL,
+                Reset = true
+            };
+            string cnd1 = Export.ExportTool.ExportCondition(cnd, "Response_0_", 0);
+            string cnd2 = Export.ExportTool.ExportCondition(cnd, "", 0);
+            MessageBox.Show($"cnd1: {Environment.NewLine}{cnd1}");
+            MessageBox.Show($"cnd2: {Environment.NewLine}{cnd2}");
         }
     }
 }
