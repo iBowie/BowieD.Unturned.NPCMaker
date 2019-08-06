eec1ebcc63b3a22900e34da8836eb06ba1ff2a23
iBowie <31174978+iBowie@users.noreply.github.com>
2019-08-06 17:26:37 +0500

    Update Mathf.cs

--- BowieD.NPCMaker/Math/Mathf.cs
+++ BowieD.NPCMaker/Math/Mathf.cs
@@ -4,75 +4,11 @@ namespace BowieD.NPCMaker.Math
 {
     public static class Mathf
     {
-        public static Int16 Clamp(Int16 minimum, Int16 maximum, Int16 value)
+        public static T Clamp<T>(T minimum, T maximum, T value) where T : IComparable
         {
-            if (value < minimum)
+            if (value.CompareTo(minimum) < 0)
                 value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static Int32 Clamp(Int32 minimum, Int32 maximum, Int32 value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static Int64 Clamp(Int64 minimum, Int64 maximum, Int64 value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static UInt16 Clamp(UInt16 minimum, UInt16 maximum, UInt16 value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static UInt32 Clamp(UInt32 minimum, UInt32 maximum, UInt32 value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static UInt64 Clamp(UInt64 minimum, UInt64 maximum, UInt64 value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static Byte Clamp(Byte minimum, Byte maximum, Byte value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static Single Clamp(Single minimum, Single maximum, Single value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
-                value = maximum;
-            return value;
-        }
-        public static Double Clamp(Double minimum, Double maximum, Double value)
-        {
-            if (value < minimum)
-                value = minimum;
-            else if (value > maximum)
+            else if (value.CompareTo(maximum) > 0)
                 value = maximum;
             return value;
         }
