b53abc222bca62c15b129b2c1d996b7c7bdaf9a3
iBowie <31174978+iBowie@users.noreply.github.com>
2019-08-10 11:43:57 +0500

    dialogue mistakes

---
+++ BowieD.Unturned.NPCMaker/Mistakes/Dialogue/NE_1001.cs
@@ -0,0 +1,37 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Dialogue
+{
+    /// <summary>
+    /// Dialogue has zero id
+    /// </summary>
+    public class NE_1001 : Mistake
+    {
+        public NE_1001() { }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            foreach (var _dial in MainWindow.CurrentProject.data.dialogues)
+            {
+                if (_dial.id == 0)
+                {
+                    yield return new NE_1001()
+                    {
+                        MistakeName = "NE_1001",
+                        Importance = IMPORTANCE.CRITICAL,
+                        MistakeDesc = LocUtil.LocalizeMistake("NE_1001_Desc", _dial.id),
+                        OnClick = new Action(() =>
+                        {
+                            if (MainWindow.DialogueEditor.Current.id == 0)
+                                return;
+                            MainWindow.DialogueEditor.Save();
+                            MainWindow.DialogueEditor.Current = _dial;
+                            MainWindow.Instance.mainTabControl.SelectedIndex = 1;
+                        })
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ BowieD.Unturned.NPCMaker/Mistakes/Dialogue/NE_1002.cs
@@ -0,0 +1,41 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Dialogue
+{
+    /// <summary>
+    /// Some messages will never appear
+    /// </summary>
+    public class NE_1002 : Mistake
+    {
+        public NE_1002() { }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            foreach (var _dial in MainWindow.CurrentProject.data.dialogues)
+            {
+                if (_dial.messages.Count >= 2)
+                {
+                    for (int k = 0; k < _dial.messages.Count - 1; k++)
+                    {
+                        if (_dial.messages[k].conditions.Length == 0)
+                            yield return new NE_1002()
+                            {
+                                MistakeName = "NE_1002",
+                                MistakeDesc = LocUtil.LocalizeMistake("NE_1002_Desc", _dial.id),
+                                Importance = IMPORTANCE.ADVICE,
+                                OnClick = new Action(() =>
+                                {
+                                    if (MainWindow.DialogueEditor.Current.id == 0)
+                                        return;
+                                    MainWindow.DialogueEditor.Save();
+                                    MainWindow.DialogueEditor.Current = _dial;
+                                    MainWindow.Instance.mainTabControl.SelectedIndex = 1;
+                                })
+                            };
+                    }
+                }
+            }
+        }
+    }
+}
