c655174a8b1291f49fbbd88dd0bfd418abde5f49
iBowie <31174978+iBowie@users.noreply.github.com>
2019-09-28 13:35:51 +0500

    Update AssemblyInfo.cs

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.3.1")]
-[assembly: AssemblyFileVersion("1.9.3.1")]
+[assembly: AssemblyVersion("1.9.3.2")]
+[assembly: AssemblyFileVersion("1.9.3.2")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
