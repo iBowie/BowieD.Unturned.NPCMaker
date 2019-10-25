ef35278eb320544b295c47a4bc25cc5247155b63
iBowie <31174978+iBowie@users.noreply.github.com>
2019-10-25 15:40:31 +0500

    added new mistake NE_0003, added translation for new feature

---
+++ BowieD.Unturned.NPCMaker/Mistakes/Character/NE_0003.cs
@@ -0,0 +1,30 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Character
+{
+    public class NE_0003 : Mistake
+    {
+        public NE_0003() { }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            foreach (var _char in MainWindow.CurrentProject.data.characters)
+            {
+                if ("<>:\"/\\|?*".Contains(_char.editorName))
+                {
+                    yield return new Mistake()
+                    {
+                        MistakeName = "NE_0003",
+                        Importance = IMPORTANCE.CRITICAL,
+                        MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_0003_Desc", _char.displayName, _char.id),
+                        OnClick = new Action(() =>
+                        {
+                            MainWindow.Instance.mainTabControl.SelectedIndex = 0;
+                        })
+                    };
+                }
+            }
+        }
+    }
+}
