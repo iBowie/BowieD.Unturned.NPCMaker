16987e8e0a4c1a9d7cc64f6c8cb602e5c7cb5622
iBowie <31174978+iBowie@users.noreply.github.com>
2019-12-09 21:30:13 +0500

    improved localization of UI

---
+++ BowieD.Unturned.NPCMaker/XAML/LocalizationConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace BowieD.Unturned.NPCMaker.XAML
+{
+    public class LocalizationConverter : IMultiValueConverter
+    {
+        private string _key;
+        public LocalizationConverter(string key)
+        {
+            this._key = key;
+        }
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string dictName = _key.Split('_')[0];
+                var dict = Localization.LocalizationManager.Current.GetDictionary(dictName);
+                return dict.Translate(string.Join("_", _key.Split('_').Skip(1)));
+            }
+            catch
+            {
+                return _key;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
