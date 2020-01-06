4ac41c6c50588ce3ee963476afc232ba2ae2a184
BowieD <31174978+iBowie@users.noreply.github.com>
2020-01-06 18:48:29 +0500

    added unturned workshop caching

---
+++ BowieD.Unturned.NPCMaker/Common/Utility/PathUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace BowieD.Unturned.NPCMaker.Common.Utility
+{
+    public static class PathUtility
+    {
+        public static bool IsUnturnedPath(string path)
+        {
+            return File.Exists(Path.Combine(path, "Unturned.exe"));
+        }
+        public static bool IsUnturnedWorkshopPath(string path)
+        {
+            return Directory.Exists(path) && path.Contains("304930") && path.Contains("workshop") && path.Contains("content");
+        }
+        public static string GetUnturnedWorkshopPathFromUnturnedPath(string unturnedPath)
+        {
+            var dirInfo = Directory.GetParent(Directory.GetParent(unturnedPath).FullName);
+            var workshop = new DirectoryInfo(Path.Combine(dirInfo.FullName, "workshop", "content", "304930"));
+            return workshop.FullName;
+        }
+    }
+}
--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.11.0.4")]
-[assembly: AssemblyFileVersion("1.11.0.4")]
+[assembly: AssemblyVersion("1.11.0.5")]
+[assembly: AssemblyFileVersion("1.11.0.5")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
