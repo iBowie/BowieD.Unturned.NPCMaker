f44eae7632381928b58e58d063a9b201226323c5
iBowie <31174978+iBowie@users.noreply.github.com>
2020-04-07 03:03:18 +0500

    Create DateTimeRange.cs

---
+++ BowieD.Unturned.NPCMaker/Data/DateTimeRange.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BowieD.Unturned.NPCMaker.Data
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public struct DateTimeRange
+    {
+        public DateTimeRange(DateTime from, DateTime to, bool annual = false)
+        {
+            this.From = from;
+            this.To = to;
+            this.Annual = annual;
+        }
+        [JsonProperty("from")]
+        public DateTime From { get; }
+        [JsonProperty("to")]
+        public DateTime To { get; }
+        [JsonProperty("annual")]
+        public bool Annual { get; }
+
+        public bool IsInRange(DateTime dateTime)
+        {
+            DateTime checkFrom, checkTo;
+
+            var fromDeltaYears = DateTime.UtcNow.Year - From.Year;
+            var toDeltaYears = DateTime.UtcNow.Year - To.Year;
+
+            if (Annual && fromDeltaYears >= 0 && toDeltaYears >= 0)
+            {
+                checkFrom = From.AddYears(fromDeltaYears);
+                checkTo = To.AddYears(toDeltaYears);
+            }
+            else
+            {
+                checkFrom = From;
+                checkTo = To;
+            }
+            return dateTime > checkFrom && dateTime < checkTo;
+        }
+    }
+}
