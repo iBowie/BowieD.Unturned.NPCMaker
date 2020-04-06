a8f9d8951ef3f3da8cebc94f178b5ab8cb8b93c1
iBowie <31174978+iBowie@users.noreply.github.com>
2020-04-07 03:03:24 +0500

    Create AppPackage.cs

---
+++ BowieD.Unturned.NPCMaker/Data/AppPackage.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Data
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public sealed class AppPackage
+    {
+        public AppPackage()
+        {
+            Credits = new Dictionary<string, string>();
+            Guides = new Dictionary<string, string>();
+        }
+
+        public const string url = "https://raw.githubusercontent.com/iBowie/BowieD.Unturned.NPCMaker/master/PACKAGE.json";
+
+        private string[] _patrons;
+        [JsonProperty("patrons")]
+        public string[] Patrons
+        {
+            get => _patrons ?? new string[0];
+            set => _patrons = value;
+        }
+        private Holiday[] _holidays;
+        [JsonProperty("holidays")]
+        public Holiday[] Holidays
+        {
+            get => _holidays ?? new Holiday[0];
+            set => _holidays = value;
+        }
+        private Dictionary<string, string> _credits;
+        [JsonProperty("credits")]
+        public Dictionary<string, string> Credits 
+        {
+            get => _credits ?? new Dictionary<string, string>();
+            set => _credits = value;
+        }
+        private Dictionary<string, string> _guides;
+        [JsonProperty("guides")]
+        public Dictionary<string, string> Guides
+        {
+            get => _guides ?? new Dictionary<string, string>();
+            set => _guides = value;
+        }
+        private FeedbackLink[] _feedback;
+        [JsonProperty("feedback")]
+        public FeedbackLink[] FeedbackLinks
+        {
+            get => _feedback ?? new FeedbackLink[0];
+            set => _feedback = value;
+        }
+    }
+}
