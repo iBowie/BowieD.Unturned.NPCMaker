a5e42dcdf3d113259c988ba17ff04f1c5f77116b
iBowie <31174978+iBowie@users.noreply.github.com>
2020-04-15 13:21:42 +0500

    refactored quest mistakes

--- BowieD.Unturned.NPCMaker/Mistakes/Quest/NE_3002.cs
+++ BowieD.Unturned.NPCMaker/Mistakes/Quest/NE_3002.cs
@@ -1,5 +1,4 @@
 ﻿using BowieD.Unturned.NPCMaker.Localization;
-using System;
 using System.Collections.Generic;
 
 namespace BowieD.Unturned.NPCMaker.Mistakes.Quest
@@ -7,28 +6,24 @@ namespace BowieD.Unturned.NPCMaker.Mistakes.Quest
     /// <summary>
     /// No rewards
     /// </summary>
-    public class NE_3002 : Mistake
+    public class NE_3002 : QuestMistake
     {
+        public NE_3002() : base()
+        {
+            MistakeName = "NE_3002";
+            Importance = IMPORTANCE.ADVICE;
+        }
+        public NE_3002(string title, ushort id) : this()
+        {
+            MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_3002_Desc", title, id);
+        }
         public override IEnumerable<Mistake> CheckMistake()
         {
             foreach (var _quest in MainWindow.CurrentProject.data.quests)
             {
                 if (_quest.rewards.Count == 0)
                 {
-                    yield return new NE_3002()
-                    {
-                        MistakeName = "NE_3002",
-                        MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_3002_Desc", _quest.title, _quest.id),
-                        Importance = IMPORTANCE.ADVICE,
-                        OnClick = new Action(() =>
-                        {
-                            if (MainWindow.Instance.MainWindowViewModel.QuestTabViewModel.ID == 0)
-                                return;
-                            MainWindow.Instance.MainWindowViewModel.QuestTabViewModel.SaveCommand.Execute(null);
-                            MainWindow.Instance.MainWindowViewModel.QuestTabViewModel.Quest = _quest;
-                            MainWindow.Instance.mainTabControl.SelectedIndex = 3;
-                        })
-                    };
+                    yield return new NE_3002(_quest.title, _quest.id);
                 }
             }
         }
---
+++ BowieD.Unturned.NPCMaker/Mistakes/Quest/QuestMistake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Quest
+{
+    public abstract class QuestMistake : Mistake
+    {
+        public QuestMistake()
+        {
+            OnClick = new Action(() =>
+            {
+                MainWindow.Instance.mainTabControl.SelectedIndex = 3;
+            });
+        }
+    }
+}
