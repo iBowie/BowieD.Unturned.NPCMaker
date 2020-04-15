cb1afc0f2abb14626b1128d7af33781b6afabc2d
iBowie <31174978+iBowie@users.noreply.github.com>
2020-04-15 14:06:36 +0500

    added bunch of new mistakes

---
+++ BowieD.Unturned.NPCMaker/Mistakes/Dialogue/NE_1007.cs
@@ -0,0 +1,36 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Dialogue
+{
+    /// <summary>
+    /// Recursive call
+    /// </summary>
+    public sealed class NE_1007 : DialogueMistake
+    {
+        public NE_1007() : base()
+        {
+            MistakeName = "NE_1007";
+            Importance = IMPORTANCE.WARNING;
+        }
+        public NE_1007(int responseId, ushort dialogueId) : this()
+        {
+            MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_1007_Desc", responseId, dialogueId);
+        }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            foreach (var dial in MainWindow.CurrentProject.data.dialogues)
+            {
+                for (int i = 0; i < dial.responses.Count; i++)
+                {
+                    var response = dial.responses[i];
+
+                    if (response.openDialogueId == dial.id)
+                    {
+                        yield return new NE_1007(i + 1, dial.id);
+                    }
+                }
+            }
+        }
+    }
+}
