6c637ef36b53a133d868d2d2a3e4a4cdba4f9adc
iBowie <31174978+iBowie@users.noreply.github.com>
2020-05-12 23:06:09 +0500

    Update AssemblyInfo.cs

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.12.0.6")]
-[assembly: AssemblyFileVersion("1.12.0.6")]
+[assembly: AssemblyVersion("1.12.1.0")]
+[assembly: AssemblyFileVersion("1.12.1.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
