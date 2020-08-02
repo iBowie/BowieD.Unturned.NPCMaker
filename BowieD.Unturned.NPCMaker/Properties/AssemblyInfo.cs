c59157f136a76ef932a579285587d947ebe7e259
iBowie <31174978+iBowie@users.noreply.github.com>
2020-08-02 16:55:48 +0500

    added markdown

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.13.0.5")]
-[assembly: AssemblyFileVersion("1.13.0.5")]
+[assembly: AssemblyVersion("1.13.0.6")]
+[assembly: AssemblyFileVersion("1.13.0.6")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
