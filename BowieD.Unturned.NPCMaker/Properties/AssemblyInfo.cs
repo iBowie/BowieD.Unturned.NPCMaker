f2d1620470d465b98036ec2de963a0c32645c377
iBowie <31174978+iBowie@users.noreply.github.com>
2020-08-10 21:34:40 +0500

    version bump

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.13.0.12")]
-[assembly: AssemblyFileVersion("1.13.0.12")]
+[assembly: AssemblyVersion("1.13.0.13")]
+[assembly: AssemblyFileVersion("1.13.0.13")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
