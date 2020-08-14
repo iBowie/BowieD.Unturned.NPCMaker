542e274edcadf99c9269c7400a34a8bfb36c78d7
iBowie <31174978+iBowie@users.noreply.github.com>
2020-08-14 17:37:40 +0500

    Update CharacterTabViewModel.cs

--- BowieD.Unturned.NPCMaker/ViewModels/CharacterTabViewModel.cs
+++ BowieD.Unturned.NPCMaker/ViewModels/CharacterTabViewModel.cs
@@ -45,6 +45,14 @@ namespace BowieD.Unturned.NPCMaker.ViewModels
             }));
 
             MainWindow.Instance.characterTabButtonAdd.ContextMenu = cmenu3;
+
+            ContextMenu cmenu4 = new ContextMenu();
+
+            cmenu4.Items.Add(ContextHelper.CreatePasteItalicButton());
+            cmenu4.Items.Add(ContextHelper.CreatePasteBoldButton());
+            cmenu4.Items.Add(ContextHelper.CreatePasteColorMenu());
+
+            MainWindow.Instance.txtDisplayName.ContextMenu = cmenu4;
         }
 
         private void CharacterTabButtonAdd_Click(object sender, RoutedEventArgs e)
