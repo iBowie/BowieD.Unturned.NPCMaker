2d988b264315828f13e1f417006221b638798469
BowieD <31174978+iBowie@users.noreply.github.com>
2020-11-24 12:22:17 +0500

    added select to vendor

--- BowieD.Unturned.NPCMaker/ViewModels/BaseCommand.cs
+++ BowieD.Unturned.NPCMaker/ViewModels/BaseCommand.cs
@@ -15,7 +15,13 @@ namespace BowieD.Unturned.NPCMaker.ViewModels
         }
         private readonly Action action;
         private readonly Action<object> actionWithParameter;
-        public event EventHandler CanExecuteChanged;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
         public virtual bool CanExecute(object parameter)
         {
             return true;
@@ -27,6 +33,54 @@ namespace BowieD.Unturned.NPCMaker.ViewModels
                 actionWithParameter.Invoke(parameter);
             else
                 action?.Invoke();
+        }
+    }
+    public class AdvancedCommand : ICommand
+    {
+        private Func<object, bool> _canExecute;
+        private Action<object> _execute;
+
+        public AdvancedCommand(Action action)
+        {
+            _execute = new Action<object>((obj) =>
+            {
+                action.Invoke();
+            });
+            _canExecute = new Func<object, bool>((obj) => true);
+        }
+        public AdvancedCommand(Action<object> action)
+        {
+            _execute = action;
+            _canExecute = new Func<object, bool>((obj) => true);
+        }
+        public AdvancedCommand(Action action, Func<object, bool> canExecute)
+        {
+            _execute = new Action<object>((obj) =>
+            {
+                action.Invoke();
+            });
+            _canExecute = canExecute;
+        }
+        public AdvancedCommand(Action<object> action, Func<object, bool> canExecute)
+        {
+            _execute = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
         }
     }
 }
