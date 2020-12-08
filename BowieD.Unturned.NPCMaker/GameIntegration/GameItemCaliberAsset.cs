4a9c70366c6ad797447834b3aa6423f10b634560
BowieD <31174978+iBowie@users.noreply.github.com>
2020-12-09 01:54:06 +0500

    reading calibers

--- BowieD.Unturned.NPCMaker/GameIntegration/GameItemCaliberAsset.cs
+++ BowieD.Unturned.NPCMaker/GameIntegration/GameItemCaliberAsset.cs
@@ -7,7 +7,14 @@ namespace BowieD.Unturned.NPCMaker.GameIntegration
     {
         public GameItemCaliberAsset(DataReader data, string dirName, string name, ushort id, Guid guid, string type, EGameAssetOrigin origin) : base(data, dirName, name, id, guid, type, origin)
         {
+            calibers = new ushort[data.ReadByte("Calibers", 0)];
 
+            for (int i = 0; i < calibers.Length; i++)
+            {
+                calibers[i] = data.ReadUInt16($"Caliber_{i}", 0);
+            }
         }
+
+        public readonly ushort[] calibers;
     }
 }
--- BowieD.Unturned.NPCMaker/GameIntegration/GameItemGunAsset.cs
+++ BowieD.Unturned.NPCMaker/GameIntegration/GameItemGunAsset.cs
@@ -11,8 +11,40 @@ namespace BowieD.Unturned.NPCMaker.GameIntegration
             hasTactical = data.Has("Hook_Tactical");
             hasGrip = data.Has("Hook_Grip");
             hasBarrel = data.Has("Hook_Barrel");
+
+            int num2 = data.ReadInt32("Magazine_Calibers", 0);
+            if (num2 > 0)
+            {
+                magazineCalibers = new ushort[num2];
+                for (int j = 0; j < num2; j++)
+                {
+                    magazineCalibers[j] = data.ReadUInt16($"Magazine_Caliber_{j}", 0);
+                }
+                int num3 = data.ReadInt32("Attachment_Calibers");
+                if (num3 > 0)
+                {
+                    attachmentCalibers = new ushort[num3];
+                    for (int k = 0; k < num3; k++)
+                    {
+                        attachmentCalibers[k] = data.ReadUInt16($"Attachment_Caliber_{k}", 0);
+                    }
+                }
+                else
+                {
+                    attachmentCalibers = magazineCalibers;
+                }
+            }
+            else
+            {
+                magazineCalibers = new ushort[1];
+                magazineCalibers[0] = data.ReadUInt16("Caliber", 0);
+                attachmentCalibers = magazineCalibers;
+            }
         }
 
         public bool hasSight, hasTactical, hasGrip, hasBarrel;
+
+        public readonly ushort[] magazineCalibers;
+        public readonly ushort[] attachmentCalibers;
     }
 }
