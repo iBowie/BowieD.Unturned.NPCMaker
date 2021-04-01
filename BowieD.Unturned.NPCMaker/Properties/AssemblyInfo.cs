db2c4c44c2ef26ccfa3603d12a39547922e34b2d
BowieD <31174978+iBowie@users.noreply.github.com>
2021-04-02 03:26:00 +0500

    revert april

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.16.0.3")]
-[assembly: AssemblyFileVersion("1.16.0.3")]
+[assembly: AssemblyVersion("1.16.0.7")]
+[assembly: AssemblyFileVersion("1.16.0.7")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
