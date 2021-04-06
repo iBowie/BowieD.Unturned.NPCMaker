c602df15dc2e8d2703a03166938c40979c98a026
BowieD <31174978+iBowie@users.noreply.github.com>
2021-04-07 00:25:32 +0500

    mistakes related to project id range

---
+++ BowieD.Unturned.NPCMaker/Mistakes/Dialogue/NE_1008.cs
@@ -0,0 +1,29 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Dialogue
+{
+    public class NE_1008 : DialogueMistake
+    {
+        public NE_1008()
+        {
+            MistakeName = "NE_1008";
+            Importance = IMPORTANCE.WARNING;
+        }
+        public NE_1008(ushort id, ushort min, ushort max) : this()
+        {
+            MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_1008_Desc", id, min, max);
+        }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            var rangeMin = MainWindow.CurrentProject.data.settings.idRangeMin;
+            var rangeMax = MainWindow.CurrentProject.data.settings.idRangeMax;
+
+            foreach (NPC.NPCDialogue _dial in MainWindow.CurrentProject.data.dialogues)
+            {
+                if (_dial.ID < rangeMin || _dial.ID > rangeMax)
+                    yield return new NE_1008(_dial.ID, rangeMin, rangeMax);
+            }
+        }
+    }
+}
---
+++ BowieD.Unturned.NPCMaker/Mistakes/Vendor/NE_2002.cs
@@ -0,0 +1,29 @@
+﻿using BowieD.Unturned.NPCMaker.Localization;
+using System.Collections.Generic;
+
+namespace BowieD.Unturned.NPCMaker.Mistakes.Vendor
+{
+    public class NE_2002 : VendorMistake
+    {
+        public NE_2002()
+        {
+            MistakeName = "NE_2002";
+            Importance = IMPORTANCE.WARNING;
+        }
+        public NE_2002(string title, ushort id, ushort min, ushort max) : this()
+        {
+            MistakeDesc = LocalizationManager.Current.Mistakes.Translate("NE_2002_Desc", title, id, min, max);
+        }
+        public override IEnumerable<Mistake> CheckMistake()
+        {
+            var rangeMin = MainWindow.CurrentProject.data.settings.idRangeMin;
+            var rangeMax = MainWindow.CurrentProject.data.settings.idRangeMax;
+
+            foreach (NPC.NPCVendor _vendor in MainWindow.CurrentProject.data.vendors)
+            {
+                if (_vendor.ID < rangeMin || _vendor.ID > rangeMax)
+                    yield return new NE_2002(_vendor.Title, _vendor.ID, rangeMin, rangeMax);
+            }
+        }
+    }
+}
