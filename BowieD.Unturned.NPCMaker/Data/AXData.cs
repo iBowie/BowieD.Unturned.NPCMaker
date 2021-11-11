9940ca3eb0fe3765aae0aa69a2eaa0d3a230926e
BowieD <31174978+iBowie@users.noreply.github.com>
2021-11-11 17:22:44 +0500

    first step to implement simulation presets

---
+++ BowieD.Unturned.NPCMaker/Data/AXData.cs
@@ -0,0 +1,107 @@
+﻿using BowieD.Unturned.NPCMaker.Common;
+using BowieD.Unturned.NPCMaker.NPC;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace BowieD.Unturned.NPCMaker.Data
+{
+    public abstract class AXData<T> : IData<T> where T : IAXData, new()
+    {
+        protected AXData() { }
+
+        public T data;
+
+        public event DataLoaded<T> OnDataLoaded;
+        public event DataSaved<T> OnDataSaved;
+
+        public abstract string FileName { get; }
+
+        public abstract string RootNodeName { get; }
+        public virtual string SaveDataNodeName => "SAVEDATA_VERSION";
+        public abstract int CurrentSaveDataVersion { get; }
+
+        public bool Save()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+
+                doc.AppendChild(doc.CreateXmlDeclaration("1.0", "utf-8", string.Empty));
+
+                XmlNode root = doc.CreateNodeC(RootNodeName, doc);
+
+                var xsiA = doc.CreateAttribute("xmlns:xsi", "http://www.w3.org/2000/xmlns/");
+                xsiA.Value = "http://www.w3.org/2001/XMLSchema-instance";
+                doc.DocumentElement.Attributes.Append(xsiA);
+
+                var xsdA = doc.CreateAttribute("xmlns:xsd", "http://www.w3.org/2000/xmlns/");
+                xsdA.Value = "http://www.w3.org/2001/XMLSchema";
+                doc.DocumentElement.Attributes.Append(xsdA);
+
+                doc.CreateNodeC(SaveDataNodeName, root).WriteInt32(CurrentSaveDataVersion);
+
+                data.Save(doc, root);
+
+                using (XmlTextWriter xtw = new XmlTextWriter(FileName, Encoding.UTF8))
+                {
+                    xtw.Formatting = Formatting.None;
+
+                    doc.Save(xtw);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                App.Logger.LogException($"[AXDATA] - Could not save to {FileName}", ex: ex);
+
+                return false;
+            }
+        }
+
+        protected abstract void GetRootAndVersion(XmlDocument document, out XmlNode root, out int version);
+
+        public bool Load(T defaultValue)
+        {
+            App.Logger.Log($"[AXDATA] - Loading {FileName}");
+
+            if (File.Exists(FileName))
+            {
+                App.Logger.Log($"[AXDATA] - Parsing XML...");
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(FileName);
+
+                try
+                {
+                    data = new T();
+
+                    GetRootAndVersion(doc, out var root, out var version);
+
+                    data.Load(root, version);
+
+                    App.Logger.Log($"[AXDATA] - Loaded");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    App.Logger.LogException($"[AXDATA] - Could not load from {FileName}", ex: ex);
+
+                    data = defaultValue;
+
+                    return false;
+                }
+            }
+            else
+            {
+                App.Logger.Log($"[AXDATA] - {FileName} does not exist. Ignoring...");
+
+                data = defaultValue;
+
+                return false;
+            }
+        }
+    }
+}
--- BowieD.Unturned.NPCMaker/NPC/Simulation.cs
+++ BowieD.Unturned.NPCMaker/NPC/Simulation.cs
@@ -1,11 +1,13 @@
-﻿using BowieD.Unturned.NPCMaker.GameIntegration;
+﻿using BowieD.Unturned.NPCMaker.Common;
+using BowieD.Unturned.NPCMaker.GameIntegration;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Xml;
 
 namespace BowieD.Unturned.NPCMaker.NPC
 {
-    public class Simulation : INotifyPropertyChanged
+    public class Simulation : INotifyPropertyChanged, IAXData
     {
         public Simulation()
         {
@@ -91,6 +93,123 @@ namespace BowieD.Unturned.NPCMaker.NPC
             }
 
             return Quest_Status.None;
+        }
+
+        public void Load(XmlNode node, int version)
+        {
+            Name = node["Name"].ToText();
+            Time = node["Time"].ToInt32();
+            Skillset = node["Skillset"].ToEnum<ESkillset>();
+            Experience = node["Experience"].ToUInt32();
+            Reputation = node["Reputation"].ToInt32();
+            Holiday = node["Holiday"].ToEnum<ENPCHoliday>();
+            Health = node["Health"].ToByte(100);
+            Food = node["Food"].ToByte(100);
+            Water = node["Water"].ToByte(100);
+            Virus = node["Virus"].ToByte(100);
+
+            var flagsNode = node["Flags"];
+
+            Flags.Clear();
+
+            foreach (XmlNode cNode in flagsNode.ChildNodes)
+            {
+                var id = cNode.Attributes["id"].ToUInt16();
+                var val = cNode.Attributes["value"].ToInt16();
+
+                Flags[id] = val;
+            }
+
+            var questsNode = node["Quests"];
+
+            Quests.Clear();
+
+            foreach (XmlNode cNode in questsNode.ChildNodes)
+            {
+                Quests.Add(cNode.ToUInt16());
+            }
+
+            var currenciesNode = node["Currencies"];
+
+            Currencies.Clear();
+
+            foreach (XmlNode cNode in currenciesNode.ChildNodes)
+            {
+                var guid = cNode.Attributes["guid"].ToText();
+                var amnt = cNode.Attributes["amount"].ToUInt32();
+
+                Currencies[guid] = amnt;
+            }
+
+            var itemsNode = node["Items"];
+
+            Items.Clear();
+
+            foreach (XmlNode cNode in itemsNode.ChildNodes)
+            {
+                var id = cNode.Attributes["id"].ToUInt16();
+                var q = cNode.Attributes["quality"].ToByte();
+                var amnt = cNode.Attributes["amount"].ToByte();
+
+                Items.Add(new Item()
+                {
+                    ID = id,
+                    Amount = amnt,
+                    Quality = q
+                });
+            }
+        }
+
+        public void Save(XmlDocument document, XmlNode node)
+        {
+            document.CreateNodeC("Name", node).WriteString(Name);
+            document.CreateNodeC("Time", node).WriteInt32(Time);
+            document.CreateNodeC("Skillset", node).WriteEnum(Skillset);
+            document.CreateNodeC("Experience", node).WriteUInt32(Experience);
+            document.CreateNodeC("Reputation", node).WriteInt32(Reputation);
+            document.CreateNodeC("Holiday", node).WriteEnum(Holiday);
+            document.CreateNodeC("Health", node).WriteByte(Health);
+            document.CreateNodeC("Food", node).WriteByte(Food);
+            document.CreateNodeC("Water", node).WriteByte(Water);
+            document.CreateNodeC("Virus", node).WriteByte(Virus);
+
+            var flagsNode = document.CreateNodeC("Flags", node);
+
+            foreach (var flag in Flags)
+            {
+                var flagNode = document.CreateNodeC("Flag", flagsNode);
+
+                document.CreateAttributeC("id", flagNode).WriteUInt16(flag.Key);
+                document.CreateAttributeC("value", flagNode).WriteInt16(flag.Value);
+            }
+
+            var questsNode = document.CreateNodeC("Quests", node);
+
+            foreach (var quest in Quests)
+            {
+                document.CreateNodeC("Quest", questsNode).WriteUInt16(quest);
+            }
+
+            var currNode = document.CreateNodeC("Currencies", node);
+
+            foreach (var currency in Currencies)
+            {
+                var c = document.CreateNodeC("Currency", currNode);
+
+                document.CreateAttributeC("guid", c).WriteString(currency.Key);
+                document.CreateAttributeC("amount", c).WriteUInt32(currency.Value);
+            }
+
+            var itemsNode = document.CreateNodeC("Items", node);
+
+            foreach (var item in Items)
+            {
+                var c = document.CreateNodeC("Item", itemsNode);
+
+                document.CreateAttributeC("id", c).WriteUInt16(item.ID);
+                document.CreateAttributeC("quality", c).WriteByte(item.Quality);
+                document.CreateAttributeC("amount", c).WriteByte(item.Amount);
+            }
         }
     }
 }
