9251ddb28c3bb3ecb3c3e0a9ab05a90124416a8d
BowieD <31174978+iBowie@users.noreply.github.com>
2022-07-10 11:31:38 +0500

    Update AssemblyInfo.cs

--- BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
+++ BowieD.Unturned.NPCMaker/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.18.0.0")]
-[assembly: AssemblyFileVersion("1.18.0.0")]
+[assembly: AssemblyVersion("1.18.0.1")]
+[assembly: AssemblyFileVersion("1.18.0.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
